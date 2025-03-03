666bd924768fb144e4155fd9786ac533df4c7c14
jakub <01197803@pw.edu.pl>
2025-03-03 23:20:35 +0100

    Dodano drugi program do obsługi LLM z Ollamy

---
+++ Ollama/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    application
+}
+
+application {
+    mainClass.set("Chatbot")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
